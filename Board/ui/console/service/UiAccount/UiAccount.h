e10009ec4fa19f37d8a08326bed1c7034d64ec09
Falcon00030 <149447216+Falcon00030@users.noreply.github.com>
2023-12-06 12:13:08 +0900

    feat:[장성영]AccountUI기능제작[SDCT-UI-10]

--- Board/ui/console/service/UiAccount/UiAccount.h
+++ Board/ui/console/service/UiAccount/UiAccount.h
@@ -5,4 +5,16 @@
 #ifndef CLASS_TEST_UIACCOUNT_H
 #define CLASS_TEST_UIACCOUNT_H
 
+#include <vector>
+#include "../../../../board/service/response/BoardResponse.h"
+
+class UiAccount {
+public:
+    virtual ~UiAccount() = default;
+    virtual std::vector<BoardResponse> UiServiceLoginHandler() = 0;
+    virtual std::vector<BoardResponse> UiServiceCreateAccountHandler() = 0;
+    virtual std::vector<BoardResponse> UiServiceExitHandler() = 0;
+
+};
+
 #endif //CLASS_TEST_UIACCOUNT_H
--- Board/ui/console/service/UiAccount/UiAccountImpl.cpp
+++ Board/ui/console/service/UiAccount/UiAccountImpl.cpp
@@ -1,3 +1,57 @@
 //
 // Created by eddi on 23. 12. 6.
 //
+
+#include <vector>
+#include <iostream>
+#include "UiAccountImpl.h"
+#include "../../../../board/service/response/BoardResponse.h"
+
+
+std::vector<BoardResponse> UiAccountImpl::UiServiceLoginHandler() {
+    std::vector<BoardResponse> boardResponses;
+    std::cout << "로그인을 해주세요" << std::endl;
+
+    std::string user;
+    std::string password;
+
+    std::cout << "\nID를 입력하세요: ";
+    std::cin >> user;
+    std::cout << "\n비밀번호를 입력하세요: ";
+    std::cin >> password;
+
+    std::cout <<"ID : "<< user << "비밀번호 : " <<password << std::endl; //확인용
+
+    bool loginSuccess = false;
+
+    if (password == "password")
+        {
+            loginSuccess = true;
+        }
+    if (loginSuccess)
+        {
+            std::cout << "로그인 성공!" << std::endl;
+
+            return boardResponses;
+        }
+    else
+        {
+            std::cout << "로그인 실패!" << std::endl;
+
+            return std::vector<BoardResponse>();
+        }
+}
+
+
+std::vector<BoardResponse> UiAccountImpl::UiServiceCreateAccountHandler()
+{
+//    std::cout << "UiBoardImpl:: UiServiceExitHandler:출력!" << std::endl;
+
+
+}
+
+
+std::vector<BoardResponse> UiAccountImpl::UiServiceExitHandler()
+{
+    std::cout << "UiBoardImpl:: UiServiceModifyHandler:출력!" << std::endl;
+}
