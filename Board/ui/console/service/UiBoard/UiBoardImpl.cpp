1a35df7a4eb121b79992b97d5e0ffadc59e863e4
bikjo99 <149447229+bikjo99@users.noreply.github.com>
2023-12-07 12:03:29 +0900

    feat: [이성용] UiBoardImpl에 modify,read,remove 구현[SDCT1-UI-11]

--- Board/ui/console/service/UiBoard/UiBoardImpl.cpp
+++ Board/ui/console/service/UiBoard/UiBoardImpl.cpp
@@ -40,16 +40,52 @@ std::vector<BoardResponse> UiBoardImpl::UiServiceExitHandler()
 std::vector<BoardResponse> UiBoardImpl::UiServiceModifyHandler()
 {
     std::cout << "UiBoardImpl:: UiServiceModifyHandler:출력!" << std::endl;
+
+    user_keyboard_input _user_keyboard_input;
+
+    std::string board_id_print = "\n수정하고 싶은 게시물 번호를 입력하세요: ";
+    std::string title_print = "\n제목을 입력하세요: ";
+    std::string writer_print = "\n작성자를 입력하세요: ";
+    std::string content_print = "\n내용을 입력하세요: ";
+
+    std::string board_id;
+    std::string title;
+    std::string writer;
+    std::string content;
+
+    _user_keyboard_input.get_user_keyboard_input_with_message(board_id_print, board_id);
+    _user_keyboard_input.get_user_keyboard_input_with_message(title_print, title);
+    _user_keyboard_input.get_user_keyboard_input_with_message(writer_print, writer);
+    _user_keyboard_input.get_user_keyboard_input_with_message(content_print, content);
+
 }
 
 
 std::vector<BoardResponse> UiBoardImpl::UiServiceReadHandler()
 {
     std::cout << "UiBoardImpl:: UiServiceReadHandler:출력!" << std::endl;
+
+    user_keyboard_input _user_keyboard_input;
+
+    std::string board_id_print = "\n읽고 싶은 게시물 번호를 입력하세요: ";
+
+    std::string board_id;
+
+    _user_keyboard_input.get_user_keyboard_input_with_message(board_id_print, board_id);
+
 }
 
 
 std::vector<BoardResponse> UiBoardImpl::UiServiceRemoveHandler()
 {
     std::cout << "UiBoardImpl:: UiServiceRemoveHandler:출력!" << std::endl;
+
+    user_keyboard_input _user_keyboard_input;
+
+    std::string board_id_print = "\n삭제하고 싶은 게시물 번호를 입력하세요: ";
+
+    std::string board_id;
+
+    _user_keyboard_input.get_user_keyboard_input_with_message(board_id_print, board_id);
+
 }
