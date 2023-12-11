d0fabcf71a90cf396fd0d2f6462346b28e74e8d2
crazyfire29 <57270706+crazyfire29@users.noreply.github.com>
2023-12-11 12:19:42 +0900

    update: [조승훈] 키보드에서 입력받는 값 오류 수정[SDCT1-UI-X]

--- Board/console/user_keyboard/user_keyboard_input.cpp
+++ Board/console/user_keyboard/user_keyboard_input.cpp
@@ -3,26 +3,27 @@
 //
 
 #include "user_keyboard_input.h"
+#include "../window/console_window.h"
 
 #include <iostream>
 #include <string>
 #include <algorithm>
 
-void user_keyboard_input::get_user_keyboard_input(std::string& keyboard_input)
-{
-    std::cin.ignore();
-    std::getline(std::cin, keyboard_input);
-
-    keyboard_input.erase(std::remove_if(keyboard_input.begin(),
-                                        keyboard_input.end(), ::isspace),
-                         keyboard_input.end());
-}
-
 void user_keyboard_input::get_user_keyboard_input_with_message(const std::string& output_message, std::string& keyboard_input)
 {
     std::cout << output_message;
+    if(!isFirst){
+        std::cin.ignore();
+    } else{
+        isFirst = false;
+    }
 
-    get_user_keyboard_input(keyboard_input);
+
+    std::getline(std::cin, keyboard_input);
+    keyboard_input.erase(
+            std::remove_if(keyboard_input.begin(),
+                           keyboard_input.end(), ::isspace),
+            keyboard_input.end());
 }
 
 user_keyboard_input::user_keyboard_input() {
